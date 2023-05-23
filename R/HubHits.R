03bbfd76ab5699ddaf2d29a72fb16543c32eb5af
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-05-23 16:22:17 -0400

    rename to HubHits and add S3 method

--- R/HubHits.R
+++ R/HubHits.R
@@ -14,15 +14,17 @@
 #'   package pattern
 #'
 #' @examples
-#' et <- HubTopicProp(
-#'     c("cancer", "tumor"), "curated*", hubtype = "AnnotationHub"
+#' et <- HubHits(
+#'     c("cancer", "tumor"), "curated*", hub = "ExperimentHub"
 #' )
+#' # proportion
+#' et[1] / et[2]
 #'
 #' @export
-HubTopicProp <- function(
+HubHits <- function(
     topics, pkg_pattern, hub = c("ExperimentHub", "AnnotationHub")
 ) {
-    hubtype <- match.arg(hub)
+    hub <- match.arg(hub)
     if (identical(hub, "ExperimentHub"))
         hub <- ExperimentHub::ExperimentHub()
     else
@@ -32,15 +34,24 @@ HubTopicProp <- function(
     ah_ids <- lapply(hublist, function(x) {
         x$ah_id
     })
-    ah_ids <- unique(unlist(ah_ids))
+    ah_ids <- unlist(ah_ids)
 
     pkgnames <- BiocManager::available(pkg_pattern)
     pkg_hub <- Filter(length, lapply(pkgnames, AnnotationHub::query, x = hub))
     pkg_ids <- lapply(pkg_hub, function(x) {
         x$ah_id
     })
-    pkg_ids <- unique(unlist(pkg_ids))
+    pkg_ids <- unlist(pkg_ids)
 
     res_ids <- unique(c(ah_ids, pkg_ids))
-    length(res_ids) / length(hub$ah_id)
+    res <- c(n.hits = length(res_ids), total = length(hub$ah_id))
+    class(res) <- "HubHits"
+    res
+}
+
+prop.HubHits <- function(HubHits) {
+    stopifnot(inherits(HubHits, "HubHits"))
+    prop <- HubHits['n.hits'] / HubHits["total"]
+    names(prop) <- "prop.hits"
+    prop
 }
